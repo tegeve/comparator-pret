a1ef6dff6e0c560a4a482f529a6df9c8b402edc8
GELU <terecgeluvlad@yahoo.com>
2022-06-08 12:21:50 +0300

    proiect final

--- scraper/models.py
+++ scraper/models.py
@@ -8,6 +8,7 @@ class Produse(models.Model):
     active = models.BooleanField(default=1)
     pret_altex = models.CharField(max_length=100)
     pret_emag = models.CharField(max_length=100)
+    imagine = models.ImageField(null=True, blank=True)
 
 
     def __str__(self):
